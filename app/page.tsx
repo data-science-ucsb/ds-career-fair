a6316b86e729b01815dcc3333f51890e6c3c8488
SwiftWinds <mateo.wang@moloco.com>
2023-08-24 22:37:41 -0700

    Fetch initial data

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,41 @@
 import { useForm } from "react-hook-form";
 
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "@/app/config";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { auth, db } from "@/app/config";
 import { signOut } from "firebase/auth";
 import { doc, updateDoc } from "firebase/firestore";
 
 import LoginButton from "@/components/LoginButton";
 import FormInput from "@/components/FormInput";
+import { useEffect, useMemo } from "react";
 
 export default function Home() {
-  const [user, loading, error] = useAuthState(auth);
+  const [user, loadingUser, error] = useAuthState(auth);
+
+  const repDoc = user && doc(db, `reps/${user.uid}`);
+
+  const [repData, loadingRep] = useDocumentData(repDoc);
+
+  const { company } = repData || {};
+
+  const companyDoc = company && doc(db, `companies/${company}`);
+
+  const [companyData, loadingCompany] = useDocumentData(companyDoc);
+
+  const defaultValues = companyData;
+
+  console.log(defaultValues);
+
+  const loading = loadingUser || loadingRep || loadingCompany;
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    defaultValues: useMemo(() => defaultValues, [defaultValues]),
+  });
 
   if (loading) return <h1>Loading...</h1>;
 
@@ -28,14 +48,15 @@ export default function Home() {
       <main>
         <h1>Welcome, {user.displayName}</h1>
         <h2>Let&apos;s find your next job</h2>
-        <form onSubmit={handleSubmit((data) => {
-          const userRef = doc(db, "users", user.uid);
-          await updateDoc(userRef, {
-            name: data.name,
-            description: data.description,
-            location: data.location,
-          });
-        })}>
+        <form
+          onSubmit={handleSubmit((data) => {
+            updateDoc(companyDoc, {
+              name: data.name,
+              description: data.description,
+              location: data.location,
+            });
+          })}
+        >
           <FormInput
             name="name"
             label="company's name"
